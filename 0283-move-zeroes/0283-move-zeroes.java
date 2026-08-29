c119c5b898e7466cfdf236fd4164086adb78f753
medeepakmishra <146239205+medeepakmishra@users.noreply.github.com>
2026-08-29 23:15:26 +0530

    Time: 232 ms (5.3%), Space: 48.1 MB (6.82%) - LeetHub - Time: O(n^2), Space: O(1)

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,26 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        for(int i =0;i<n-1;i++){
 
-        int i = 0;
+            for(int j =0; j<n-1-i;j++){
+                if(nums[j]==0){
+                    int temp = nums[j];
+                    nums[j]= nums[j+1];
+                    nums[j+1]= temp;
 
-        for (int j = 0; j < nums.length; j++) {
-
-            if (nums[j] != 0) {
-
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-
-                i++;
+                }
             }
+
+
+           
         }
+
+        
+        
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
