c4b1ab0d11839d0144374f084435f34b91cc4d45
medeepakmishra <146239205+medeepakmishra@users.noreply.github.com>
2026-08-31 21:45:46 +0530

    Time: 5 ms (83.23%), Space: 77.4 MB (49.18%) - LeetHub - Time: O(n), Space: O(1)

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,31 +1,32 @@
 class Solution {
     public int maxArea(int[] height) {
 
-        int i = 0, j=height.length-1, area = 0,currentArea =0;
+        int max = 0;
+        int i=0, j=height.length-1;
 
         while(i<j){
+           int width = j-i;
+           int ht = Math.min(height[i],height[j]);
+           int currentWater = width*ht;
+           max = Math.max(max, currentWater);
+           
+           if(height[i]<height[j]){
+            i++;
+           }else{
+            j--;
+           }
 
-            if(height[i]>height[j]){
 
-                currentArea= height[j]*(j-i);
-                area = Math.max(area, currentArea);
-                j--;
 
-            }else{
-                
-                currentArea= height[i]*(j-i);
-                area = Math.max(area, currentArea);
-                i++;
-
-            }
 
         }
 
-        return area;
-
-
-
-
+        return max;
+        
         
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
